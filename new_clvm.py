7e6c54692ef01332255f7cce35aefeddfc3d7a26
Cheng Xie <cheng.k.xie@gmail.com>
2019-04-09 22:36:41 -0400

    Updating new clvm

--- new_clvm.py
+++ new_clvm.py
@@ -1,3 +1,16 @@
+import numpy as np
+import torch as t
+import torch.nn as nn
+import torch.nn.functional as f
+import torch.optim as opt
+from torch import FloatTensor as FT
+from variational_methods import *
+from decoders import *
+from mnist_data import *
+from viz import *
+import time
+import matplotlib.pyplot as plt
+
 #G_STORE_DEVICE = torch.device('cpu')
 G_COMP_DEVICE = torch.device('cpu')
 
@@ -18,41 +31,58 @@ class DiagGaussArrayLatentVars():
         self.mean.requires_grad = self._requires_grad
         self.log_var.requires_grad = self._requires_grad
 
+    def params(self, log_var_reduction = 0):
+        return (self.mean, self.log_var)
+
     def kl_divergence(self, other_latent):
         if not type(other_latent) is DiagGaussArrayLatentBatch:
             raise NotImplementedError()
         else:
-            #TODO
+            gauss_kl_div(self.params(), other_latent.params())
 
-    def sample(self, var_reduction):
-        gauss_samp
+    def log_likelihood(self, data):
+        gauss_log_p(self.params(),data)
+
+    def sample(self, log_var_reduction = 0):
+        return gauss_samp(self.params.params(log_var_reduction=log_var_reduction))
 
     @staticmethod
     def get_normal(dim, device, requires_grad):
         return DiagGaussArrayLatentVars(
-                mean = t.zeros(dim, device = device), 
-                log_var = t.zeros(dim, device = device), 
+                mean = t.zeros(dim, device = device),
+                log_var = t.zeros(dim, device = device),
                 requires_grad)
-            
-class DiagGaussArrayLatentStore:
 
+
+class DiagGaussArrayLatentStore:
     def __init__(self, dim, num):
         # Init to normal
         self.dim = dim
         self.mean = np.zeros(dim,np.float32)
         self.log_var = np.zeros(dim,np.float32)
         self.var_dict = {"mean": self.mean, "log_var": self.log_var}
+        self._optimizer = None
 
     def load_batch(indices, device, requires_grad = False):
         # Loads batch of values for computation
         mean_batch = self.mean[indices]
         log_var_batch = self.log_var[indices]
-        return DiagGaussArrayLatentBatch(self, mean_batch, log_var_batch, 
+        return DiagGaussArrayLatentBatch(self, mean_batch, log_var_batch,
                                      requires_grad = requires_grad)
+
+    def register_optimizer(self, optimizer):
+        self._optimizer = optimizer
+
+    def optimizer(self):
+        return self._optimizer
 
 
 class AdamLatentOpt:
     def __init__(self, latent_var, params):
+        self.latent_var = latent_var
+        # Register self as optimizer
+        self.latent_var.optimizer = self
+
         # Create m0 and m1 arrays
         self.params = params
         self.m0s = {}
@@ -75,74 +105,122 @@ class AdamLatentOpt:
             b_m1 = self.m1s[k][indices]/(1-b2)
             self.latent_var.var_dict[k][indices] -= lr*b_m0/(np.sqrt(b_m1)+e)
 
+
 class MiddleEdge:
     # Edge between two latents
     def __init__(self, model_class, model_params, output_latent_store):
         # Returns what the input_dim would have to be
-        # Model dictates what the input_latent has to be
         output_dim = output_latent_store.dim
-        # Check if this is valid 
+        # Check if this is valid
         model = model_class(model_params)
         assert(model.is_valid_output_dim(output_dim))
         self.model = model
+        # Model dictates what the input_latent has to be
         return self.model.get_required_input_dim(output_dim)
 
-    def lp_loss(input_latent_batch, output_latent_batch):
+    def kl_div(input_latent_batch, output_latent_batch):
         # Corresponding batches of input and output latent variables
         # Sample z_in from q_in
         q_in_samp = input_latent_batch.sample()
 
         # Compute p(z_out|z_in)
-        p_out = output_posterior(q_in_samp)
+        p_out = output_likelihood(q_in_samp)
 
         # Compute KL(p_out||q_out)
         q_out = output_latent_batch
         kl_q_p = p_out.kl_divergence(q_out)
-        loss = -kl_q_p 
+        return kl_q_p
 
-        return loss
-
-    def output_posterior(input_sample):
+    def output_likelihood(input_sample):
         # Compute p(z_out|z_in)
         p_out_mean, p_out_log_var = self.model(input_sample)
         p_out = DiagGaussArrayLatentVars(p_out_mean, p_out_log_var)
 
         return p_out
-        
+
+
 class BottomEdge:
     # Edge between bottom latent and gt data
-    def __init__(self, output_dim):
+    def __init__(self, model_class, model_params, output_dim):
+        # Returns what the input_dim would have to be
+        # Check if this is valid
+        model = model_class(model_params)
+        assert(model.is_valid_output_dim(output_dim))
+        self.model = model
+        # Model dictates what the input_latent has to be
+        return self.model.get_required_input_dim(output_dim)
 
-    def reconstruct_loss(input_latent_batch, output_data_batch):
+    def lp_loss(input_latent_batch, output_data_batch):
         # Corresponding batches of input and output latent variables
         # Sample z_in from q_in
         q_in_samp = input_latent_batch.sample()
 
-        # Compute p(z_out|z_in)
-        p_out = output_posterior(q_in_samp)
+        # Compute likelihood p(x_out|z_in)
+        p_out = output_likelihood(q_in_samp)
 
-        # Compute KL(p_out||q_out)
-        q_out = output_latent_batch
-        kl_q_p = p_out.kl_divergence(q_out)
-        loss = -kl_q_p 
+        # Compute log likelihood of the data
+        lp_q_p = -p_out.log_likelihood(output_data_batch)
 
         return loss
 
+    def output_likelihood(input_sample):
+        # Compute p(z_out|z_in)
+        p_out_mean, p_out_log_var = self.model(input_sample)
+        p_out = DiagGaussArrayLatentVars(p_out_mean, p_out_log_var)
 
-class CLVM_Chain:
-    # Construct the chain starting from the bottom
-    def __init__(self, edges):
-
-    def update_latent():
+        return p_out
 
 
-    def update_edge():
+class CLVM_Stack:
+    # Construct the stack starting from the bottom
+    def __init__(self, data):
+        self_.edges = []
+        self_.latents = []
+        self._edge_dep = {}
+        self._latent_dep = {}
 
-        prev_latent
+    def stack_latent(self, model_class, model_params)
+        if len(self._edges) == 0:
+            BottomEdge()
+        else:
+            MiddleEdge()
+
+        # Create input latent for bottom edge
+        edges[0].
+        self._edge_dep[edges[0]] = []
+
+    def update_latent(self, latent):
+        ingoing_edge, outgoing_edge = self._latent_dep[latent]
+        loss, mid_grad_kl = self.kl_loss(layer, index, extra)
+        loss, mid_grad_lp = self.lp_loss(layer, index, extra)
+
+    def update_edge(self, edge):
+        input_latent, output_latent = self._edge_dep[edge]
+        loss = self.lp_loss(layer, index, extra)
 
     def prior_loss(latent, prior):
         # Computes KL divergence
         q_z = latent
         p_z = prior
         return -q_z.kl_divergence(p_z)
+
+
+class MNISTData():
+    def __init__():
+
+
+    def load_batch(indices, device, requires_grad = False):
+
+
+def main():
+    # MNIST Test
+    edges = []
+    bot_edge = BottomEdge()
+    edges.append(bot_edge)
+    edges.append(MiddleEdge)
+    clvm = CLVM_Chain()
+
+
+if __name__ == "__main__":
+    main()
 
